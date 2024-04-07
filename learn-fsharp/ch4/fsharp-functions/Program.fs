c1ff1c5ba06503f0256624fecef8ea6621ef1ea3
k-ichirof <abachoppa1892@gmail.com>
2024-04-07 21:37:30 +0900

    演習

--- learn-fsharp/ch4/fsharp-functions/Program.fs
+++ learn-fsharp/ch4/fsharp-functions/Program.fs
@@ -4,9 +4,9 @@ printfn "%i" (add 2 3)
 
 
 // 以下のように書いてもok
-// let cardFace (card: int): string =
+// let cardFace_ (card: int): string =
 
-let cardFace (card) = 
+let cardFace_ (card) = 
   let no = card % 13
   if no = 1 then "Ace"
   elif no = 0 then "King"
@@ -14,7 +14,7 @@ let cardFace (card) =
   elif no = 11 then "Jack"
   else string no
 
-printfn "%s" (cardFace 11)
+printfn "%s" (cardFace_ 11)
 
 printfn "###########"
 printfn "コンポジション"
@@ -37,3 +37,35 @@ let sort (l: int list) = List.sort l
 let print (l: int list) = List.iter(fun x-> printfn "%i" x) l
 
 list |> sort |> print
+
+printfn "###########"
+printfn "演習"
+printfn "###########"
+
+let cards = [21; 3; 1; 7; 9; 23]
+let cardFace card = 
+    let no = card % 13
+    if no = 1 then "Ace"
+    elif no = 0 then "King"
+    elif no = 12 then "Queen"
+    elif no = 11 then "Jack"
+    else string no
+
+let suit card =
+    let no = card / 13
+    if no = 0 then "Hearts"
+    elif no = 1 then "Spades"
+    elif no = 2 then "Diamonds"
+    else "Clubs"
+
+let shuffle ls =
+  let random = System.Random()
+  ls |> List.sortBy(fun x -> random.Next())
+
+let printCard x = printfn "%s of %s" (suit x) (cardFace x)
+
+let printAll ls = List.iter(fun x -> printCard x) ls
+
+let take (no:int) (ls) = List.take  no ls
+
+cards |> shuffle |> take 3 |> printAll
