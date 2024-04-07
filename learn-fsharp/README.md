c1d2d12aef6b608c616dee2bb554a13a4c5a0ddf
k-ichirof <abachoppa1892@gmail.com>
2024-04-07 19:18:00 +0900

    F#はじめ

---
+++ learn-fsharp/README.md
@@ -0,0 +1,5 @@
+[F# の最初のステップ](https://learn.microsoft.com/ja-jp/training/paths/fsharp-first-steps/)
+
+```
+dotnet run --project fsharp-functions
+```
---
+++ learn-fsharp/ch4/fsharp-functions/Program.fs
@@ -0,0 +1,16 @@
+﻿let add a b = a + b 
+
+printfn "%i" (add 2 3)
+
+
+// let cardFace (card: int): string =
+// と書いてもok
+let cardFace (card) = 
+  let no = card % 13
+  if no = 1 then "Ace"
+  elif no = 0 then "King"
+  elif no = 12 then "Queen"
+  elif no = 11 then "Jack"
+  else string no
+
+printfn "%s" (cardFace 11)
