a26b5f94bebbc69b5be2fa269def60d32236874f
k-ichirof <abachoppa1892@gmail.com>
2024-05-12 20:20:01 +0900

    引数として関数を受け取ることに慣れたい

---
+++ grokkingfp/04_ch/02.scala
@@ -0,0 +1,15 @@
+val data = List("scala", "rust", "ada")
+
+// data.map(len)
+def len(x: String): Int = x.length
+
+// data.map(countS)
+def countS(x: String): Int = x.length - x.replace("s", "").length
+
+val nums = List(1, -10, 2, 0)
+
+// nums.map(convertPN)
+def convertPN(x: Int): Int = x * -1
+
+// nums.map(db)
+def db(x: Int): Int = x * 2
