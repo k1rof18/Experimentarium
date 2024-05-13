08a06eb9263d065e5bb9d5a7c9650775c0b15afe
k-ichirof <abachoppa1892@gmail.com>
2024-05-13 20:32:52 +0900

    val wordsWithScoreThan: Int => List[String] = highScoreWords(words, wordScore)
    イミュータブルではあるけど再利用性はどうなんかな。

--- grokkingfp/04_ch/01.scala
+++ grokkingfp/04_ch/01.scala
@@ -10,20 +10,21 @@ def wordScore(word: String): Int = {
   return score(word) + bonus(word) - penalty(word)
 }
 
-val data = List("ada", "haskell", "scala", "java", "rust")
-// rankedWords(data, wordScore)
+val words = List("ada", "haskell", "scala", "java", "rust")
+// rankedWords(words, wordScore)
 def rankedWords(words: List[String], wordScore: String => Int): List[String] = {
   words.sortBy(wordScore).reverse
 }
 
-// wordScores(data, wordScore)
+// wordScores(words, wordScore)
 def wordScores(words: List[String], wordScore: String => Int): List[Int] = {
   words.map(wordScore)
 }
 
-def isPlusNum(wordScore: Int) = wordScore > 0
-
-// highestWord(data, wordScore)
-def highestWord(words: List[String], wordScore: String => Int): List[String] = {
-  words.filter(w => isPlusNum(wordScore(w)))
+def highScoreWords(words: List[String], wordScore: String => Int): Int => List[String] = {
+  higherThan => words.filter(w => wordScore(w) > higherThan)
 }
+
+val wordsWithScoreThan: Int => List[String] = highScoreWords(words, wordScore)
+
+// wordsWithScoreThan(1))
