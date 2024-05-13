c5c0978aca69a5ce799fcf318a868429bd374986
k-ichirof <abachoppa1892@gmail.com>
2024-05-13 20:42:11 +0900

    湿度高い

--- grokkingfp/04_ch/03.scala
+++ grokkingfp/04_ch/03.scala
@@ -10,12 +10,17 @@ def s(x: String) = (x.length() - x.replace("s", "").length()) >= 3
 
 def fl(l: List[String], len: String => Boolean) = l.filter(len)
 
-val nums = List(1,2,3,41,777)
+val nums = List(5,1,2,4,0)
 
 // fln(nums, odd)
 def odd(x: Int) = x % 2 != 0
 
 // fln(nums, ov4)
 def ov4(x: Int) = x > 4
+
+def largerThan(n: Int): Int => Boolean = i => i > n
+
+// nums.filter(largerThan(1))
+// nums.filter(largerThan(4))
 
 def fln(l: List[Int], len: Int => Boolean) = l.filter(len)
