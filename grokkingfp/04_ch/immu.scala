c72d994ed8c09cea25c39d8e9139d7fc4797aea5
k-ichirof <abachoppa1892@gmail.com>
2024-05-28 21:29:27 +0900

    直積型

---
+++ grokkingfp/04_ch/immu.scala
@@ -0,0 +1,8 @@
+case class ProgrammingLang(name: String, year: Int)
+
+var java = ProgrammingLang("Java", 1995)
+var scala = ProgrammingLang("Scala", 2004)
+
+var langs = List(java, scala)
+
+// langs.filter(l => l.year > 2000)
